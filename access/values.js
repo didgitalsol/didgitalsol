0aa767e0d5ca176a33ef63298fdd5bd83770a82a
didgitalsol <cybersec_ant@proton.me>
2025-05-28 09:44:29 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "15.04.2002"; // Дата рождения
+var fullname = "Ткаченко Андрій Миколайович" // ФИО
+var name = "Андрій" // Имя
+
+// Паспорт
+var passport_id = "003416286" // Номер паспорта
+
+// КПП
+var kpp_id = "5223116754" // ИНН
+
+// Права
+var rights_valid_until = "07.02.2026" // Права "Дійсні до"
+var rights_categories = "A" // Права "Категорії"
+var rights_tsc = "ТСЦ 8632" // Права "Видав
+var rights_id = "ТАE918622" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "11.06.2024" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
